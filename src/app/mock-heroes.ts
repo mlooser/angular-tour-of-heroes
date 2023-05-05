21bc7f333b9fd4abacf57b8d1084e5dd1048399a
mlooser <mlooser@gmail.com>
2023-05-05 11:01:53 +0200

    Heroes list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from "./hero";
+
+export const HEROES : Hero[] = [
+    {id: 1, name: 'Iron Man'},
+    {id:2, name: 'Hulk'},
+    {id:3, name: 'Thor'},
+    {id:4, name: 'Dr Strange'},
+    {id:5, name: 'Spiderman'},
+];
